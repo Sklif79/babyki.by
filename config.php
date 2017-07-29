e34661ef568ff0971c482ebaa010c50b69151562
sklif <sklif_@mail.ru>
2017-07-29 12:40:57 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://babyki.mebelgood.by/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://babyki.mebelgood.by/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/');
+define('DIR_IMAGE', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/image/');
+define('DIR_LANGUAGE', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/config/');
+define('DIR_CACHE', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/mebelgood.by/babyki.mebelgood.by/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mebel_sklif');
+define('DB_PASSWORD', 'K@6757848');
+define('DB_DATABASE', 'mebelav_by_babyki');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
